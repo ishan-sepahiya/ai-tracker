78b76b2657a8e133eea009a3bdbf074a93019b95
ishan-sepahiya <isepahiya@outlook.com>
2026-03-28 22:56:58 +0530

    connecting tailwind part 3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+// next.config.js
 const nextConfig = {
   experimental: {
-    turbo: false, // 🚨 THIS FIXES TAILWIND
+    turbo: false,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
